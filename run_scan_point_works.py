193217dc55fcfd793c4033c1b31c78ec92a82848
Semen <semen.luscheev@bk.ru>
2025-02-18 21:17:45 +0300

    xtb accuracy increased to prevent problems with it

--- run_scan_point_works.py
+++ run_scan_point_works.py
@@ -9,6 +9,6 @@ while 1:
     if not os.path.exists(wpath):
         break
     os.chdir(wpath)
-    if(k>13):
-        optTS(xyz_path=os.path.join("to_opt.xyz"), threshold_rel=8, threshold_force=0.0001, mirror_coef=0.4, print_output=True, maxstep=10**4, programm=dict(name="xtb", force_constant= 6, acc=0.05),do_preopt=True,step_along=0)
+    if(k>0):
+        optTS(xyz_path=os.path.join("to_opt.xyz"), threshold_rel=8, threshold_force=0.0001, mirror_coef=0.4, print_output=True, maxstep=10**4, programm=dict(name="xtb", force_constant= 6, acc=0.0001),do_preopt=True,step_along=0)
     k+=1
